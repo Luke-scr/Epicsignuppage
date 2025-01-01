fa73b2d680d2601aa473e9fc3abc519e523b0edb
Luke <lukaaltacclol@gmail.com>
2025-01-01 17:17:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function magic(){
+    const btn = document.createElement("button")
+    const text = document.createTextNode("User domofried415 is already using that password, please try a new one!")
+    btn.appendChild(text)
+    const element = document.getElementById("div")
+    return element.appendChild(btn)
+}
