264e0fbf9bcda2af411563cd342b35137f3978a9
Luke <lukaaltacclol@gmail.com>
2025-01-02 17:36:25 +0100

    Kill main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
 
-function magic(){
-    const btn = document.createElement("button")
-    const text = document.createTextNode("User epicgamer2015gaming is already using that password, please try a new one!")
-    btn.appendChild(text)
-    const element = document.getElementById("div")
-    return element.appendChild(btn)
-}
